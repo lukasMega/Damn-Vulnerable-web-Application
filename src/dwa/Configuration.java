a8c9dcee1b0adfd761f48898e2f8b85532c86275
Bc. Lukáš Melega <lukas.melega@student.tuke.sk>
2015-04-27 10:13:32 +0200

    change database path to my local specific path

--- src/dwa/Configuration.java
+++ src/dwa/Configuration.java
@@ -5,7 +5,7 @@ package dwa;
  */
 public class Configuration {
     public static final String DRIVER_CLASS = "org.sqlite.JDBC";
-    public static final String URL = "jdbc:sqlite:/tmp/dwa.db";
+    public static final String URL = "jdbc:sqlite:D:\\Dropbox\\Dev\\IDEA projects\\web-security-training\\dwa\\dwa.db";
 //    public static final String USER = "admin";
 //    public static final String PASSWORD = "password";
 }
